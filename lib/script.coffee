1585db4601b4f77f68b5e1f04554810c88872545
Kyle Conkright <kyleconkright@gmail.com>
2014-09-18 16:54:17 -0700

    Updated CSS
    Added Controls for all networks. Need to convert them into templates.

--- lib/script.coffee
+++ lib/script.coffee
@@ -1,1 +1,4 @@
 $ ->
+    $('div.params').on 'click','a', ->
+        $(@).parent().find($('a')).removeClass 'select'
+        $(@).addClass 'select'
--- tmp/script.js
+++ tmp/script.js
@@ -1,4 +1,9 @@
 (function() {
-  $(function() {});
+  $(function() {
+    return $('div.params').on('click', 'a', function() {
+      $(this).parent().find($('a')).removeClass('select');
+      return $(this).addClass('select');
+    });
+  });
 
 }).call(this);
